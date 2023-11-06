8cd8bac332f9dc324ab84316cad2bfa6bd04ac20
maha faiz <mahafayez02@gmail.com>
2023-11-06 08:51:26 +0200

    adding the project's folders

---
+++ js/news.js
@@ -0,0 +1,22 @@
+
+let menu=document.querySelector(".menu");
+let nav=document.querySelector("nav");
+let close=document.querySelector(".close");
+
+
+
+menu.onclick=function(){
+    nav.classList.toggle("active");
+    
+   
+}
+
+
+close.onclick=function(){    
+   
+    nav.classList.toggle("active");
+   
+    }
+
+    
+ 
